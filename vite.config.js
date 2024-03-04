55a5682161c18a34a869bfdcc4f9e282a28ef56a
Enzo Cosson <enzolemercier@gmail.com>
2024-03-04 12:07:43 +0100

    fix : resolve conflicts with develop branch

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from "vite";
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  const base = mode === "production" ? "/movies-app/" : "./";
+  const base = mode === "production" ? "/travaux/vue-js/" : "./";
 
   return defineConfig({
     base,

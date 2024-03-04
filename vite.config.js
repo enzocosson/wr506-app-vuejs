9d5e84d4942e6306d9421f0e0250512f939d374d
Enzo Cosson <enzolemercier@gmail.com>
2024-03-04 15:14:14 +0100

    fix : resolve problem vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+export default ({ mode }) => {
+  const base = mode === "production" ? "/travaux/vue-js/" : "./";
+
+  return defineConfig({
+    base,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  });
+};
